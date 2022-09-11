70497c34e97943aa12190cbd7c34694d3419bc71
ania221B <abuwaj@gmail.com>
2022-09-11 23:58:10 +0200

    tab functionality on click added

---
+++ js/tabs.js
@@ -0,0 +1,66 @@
+/* ================== */
+/* VARIALBES           */
+/* ================ */
+
+const tabList = document.querySelector('[role="tablist"]')
+const tabs = [...tabList.children]
+
+/* ================== */
+/* FUNCTIONS           */
+/* ================ */
+
+/**
+ * Hides content that shouldn't curently be visible
+ * @param {HTMLElement} parent parent element of elements to hide
+ * @param {String} selector string for selecting all elements to hide
+ */
+function hideContent (parent, selector) {
+  const targetElements = [...parent.querySelectorAll(selector)]
+
+  targetElements.forEach(element => element.setAttribute('data-visibility', false))
+}
+
+/**
+ * Displays target content
+ * @param {HTMLElement} parent parent of target element
+ * @param {String} selector string for selecting target element
+ */
+function showContent (parent, selector) {
+  parent.querySelector(selector).setAttribute('data-visibility', true)
+}
+
+/* ================== */
+/* EXECUTION            */
+/* ================ */
+
+tabList.addEventListener('keydown', e => {
+  const leftArrow = 37
+  const rightArrow = 39
+
+  if (e.keyCode === leftArrow || e.keyCode === rightArrow) {
+    // tabs.forEach(t => t.setAttribute('tabindex', '-1'))
+  }
+})
+
+tabList.addEventListener('click', e => {
+  const tab = e.target.closest('button[role="tab"]')
+
+  if (!tab) return
+  const content = tab.getAttribute('aria-controls')
+  const image = tab.getAttribute('data-image')
+
+  const parentContainer = tab.parentElement
+  const mainContainer = parentContainer.parentElement
+
+  /* switch selected tab */
+  tabs.forEach(t => t.setAttribute('aria-selected', false))
+  tab.setAttribute('aria-selected', true)
+
+  /* switch tab content */
+  hideContent(mainContainer, '[role = "tabpanel"]')
+  showContent(mainContainer, `#${content}`)
+
+  /* switch tab image */
+  hideContent(mainContainer, '.tab-image')
+  showContent(mainContainer, `#${image}`)
+})
