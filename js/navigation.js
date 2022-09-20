97ae4ca5b0c4758059f7bc5b5443f098692de8a7
ania221B <abuwaj@gmail.com>
2022-09-20 21:57:05 +0200

    menu closes on click outside the menu

--- js/navigation.js
+++ js/navigation.js
@@ -3,12 +3,24 @@ const nav = document.querySelector('.primary-nav')
 
 toggleBtn.addEventListener('click', e => {
   const expanded = toggleBtn.getAttribute('aria-expanded')
-
   if (expanded === 'false') {
     toggleBtn.setAttribute('aria-expanded', true)
     nav.setAttribute('data-visibility', true)
   } else {
     toggleBtn.setAttribute('aria-expanded', false)
     nav.setAttribute('data-visibility', false)
+  }
+})
+
+document.body.addEventListener('click', e => {
+  const expanded = toggleBtn.getAttribute('aria-expanded')
+  if (expanded === 'true') {
+    if (!e.target.closest('.primary-nav') && !e.target.closest('.mobile-nav-toggle')) {
+      toggleBtn.setAttribute('aria-expanded', false)
+      nav.setAttribute('data-visibility', false)
+    } else {
+      toggleBtn.setAttribute('aria-expanded', true)
+      nav.setAttribute('data-visibility', true)
+    }
   }
 })
